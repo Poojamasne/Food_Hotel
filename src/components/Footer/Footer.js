6bf7ae5c0ffdc5ced8a50ce5c3ebaecc16ab08b3
Pooja Masne <poojamasne44@gmail.com>
2026-01-16 11:52:09 +0530

    menu pages changes fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,11 +20,12 @@ const Footer = () => {
             Experience the taste of tradition with modern hospitality.
           </p>
           <div className="social-icons">
-            <a href="#"><FaFacebook /></a>
-            <a href="#"><FaInstagram /></a>
-            <a href="#"><FaTwitter /></a>
-            <a href="#"><FaYoutube /></a>
-          </div>
+  <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+  <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+  <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+  <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+</div>
+
         </div>
 
         {/* Quick Links */}
